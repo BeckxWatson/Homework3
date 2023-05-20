b2c3adb417caa94893b6e68c8b58243c5d8334ad
Kittichai <Kittichai.kitt@mail.kmutt.ac.th>
2023-05-20 22:19:58 +0700

    Homework3

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool continueProgram = true;
+
+        while (continueProgram)
+        {
+            Console.Write("Please enter sign: ");
+            char inputChar = char.Parse(Console.ReadLine());
+
+            if (inputChar == '+' || inputChar == '-')
+            {
+                Console.Write("Please enter the size of the matrix.: ");
+                int matrixSize = int.Parse(Console.ReadLine());
+
+                Console.Write("Please enter the member of the matrix.: ");
+                double matrixElement = double.Parse(Console.ReadLine());
+
+                double result = 0;
+
+                if (inputChar == '+')
+                {
+                    result = matrixSize + matrixElement;
+                }
+                else if (inputChar == '-')
+                {
+                    result = matrixSize - matrixElement;
+                }
+
+                Console.WriteLine("Total: " + result);
+            }
+            else
+            {
+                break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
